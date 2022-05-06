d50518f213a2597d49b08c1ee05c1b3ef887688f
dolodoodle <dltokidoki@gmail.com>
2022-05-05 22:22:42 -0700

    aadded fast forward and slowmo buttons

--- j/main.js
+++ j/main.js
@@ -8,8 +8,30 @@ letsRumble.volume = 0.5;
 
 smb.volume = 0.5;
 
+
 document.getElementById('stop').addEventListener('click', (e)=> {
     letsRumble.pause();
     letsRumble.currentTime = 0;
 });
 
+
+let play = document.getElementById("play");
+let pause = document.getElementById("pause");
+let fast = document.getElementById("fast");
+let slow = document.getElementById("slow");
+
+play.addEventListener('click', (e)=> {
+    smb.play()
+});
+ 
+pause.addEventListener('click', (e)=> {
+    smb.pause()
+});
+
+fast.addEventListener('click', (e)=> {
+    smb.playbackRate = 2;
+});
+
+slow.addEventListener('click', (e)=> {
+    smb.playbackRate = .5;
+});
